aaa110f7a3bd9cfdf59416454ecafa24ebc4a0f9
Ana Ceban <ana.ceban@amdaris.com>
2024-05-02 15:38:45 +0300

    Remove azure func project

--- Amdaris.Internship.Api/Amdaris.Internship.Data/obj/Debug/net8.0/Amdaris.Internship.Data.AssemblyInfo.cs
+++ Amdaris.Internship.Api/Amdaris.Internship.Data/obj/Debug/net8.0/Amdaris.Internship.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Amdaris.Internship.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f417c18bedc5a793b2ba5e105edc6600ec468c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Amdaris.Internship.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Amdaris.Internship.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
