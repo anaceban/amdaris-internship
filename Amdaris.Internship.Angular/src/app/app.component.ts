a6c9f8ff901003ee7816010f7bb9f48324bfd24d
Kf-VladimirPlesacov <109650458+Kf-VladimirPlesacov@users.noreply.github.com>
2024-05-03 14:55:21 +0300

    Create template navigation bar

--- Amdaris.Internship.Angular/src/app/app.component.ts
+++ Amdaris.Internship.Angular/src/app/app.component.ts
@@ -1,13 +1,52 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+import { CoupensComponent } from './coupens/coupens.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MediaComponent } from './media/media.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProductsComponent } from './products/products.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { CommonModule } from '@angular/common';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { CdkMenuModule } from '@angular/cdk/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    BodyComponent,
+    CdkMenuModule,
+    CoupensComponent,
+    DashboardComponent,
+    MediaComponent,
+    OverlayModule,
+    PagesComponent,
+    ProductsComponent,
+    RouterOutlet,
+    SettingsComponent,
+    SidenavComponent,
+    StatisticsComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Amdaris.Internship.Angular';
+  isSideNavCollapsed = false;
+  screenWidth = 0;
+
+  public onToggleSideNav(data: SideNavToggle): void {
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+  }
 }
---
+++ Amdaris.Internship.Angular/src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,92 @@
+import {
+  Component,
+  Output,
+  EventEmitter,
+  OnInit,
+  HostListener,
+} from '@angular/core';
+import { navbarData } from './nav-data';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import {
+  animate,
+  keyframes,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
+
+@Component({
+  selector: 'app-sidenav',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('350ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 0 }),
+        animate('350ms', style({ opacity: 1 })),
+      ]),
+    ]),
+    trigger('rotate', [
+      transition(':enter', [
+        animate(
+          '1000ms',
+          keyframes([
+            style({ transform: 'rotate(0deg)', offset: '0' }),
+            style({ transform: 'rotate(2turn)', offset: '1' }),
+          ])
+        ),
+      ]),
+    ]),
+  ],
+})
+export class SidenavComponent implements OnInit {
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
+
+  collapsed = false;
+  screenWidth = 0;
+  navData = navbarData;
+
+  @HostListener('window:resize', ['$event'])
+  public onResize(event: any): void {
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth <= 768) {
+      this.collapsed = false;
+      this.onToggleSideNav.emit({
+        collapsed: this.collapsed,
+        screenWidth: this.screenWidth,
+      });
+    }
+  }
+
+  public ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+  }
+
+  public toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
+
+  public closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
+}
